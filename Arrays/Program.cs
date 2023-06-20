262c563fde3d40292c0985cb71788268945a15f0
bjjumba <ericjjumba5@gmail.com>
2023-06-20 18:05:57 +0300

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ArraySample{
+    class Sample{
+        static void Main(String[] args){
+
+            int [,] sample=new int [2,3]{
+                {1,2,3},
+                {4,5,6}
+            };
+            /*iterate through the arrays*/
+
+            for(int i=0;i<sample.GetLength(0);++i){
+                for(int j=0;i<sample.GetLength(1);++j){
+                    Console.Write($"{sample[i, j]} ");
+                }
+            }
+           Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=12;
+            int hex = 0x2A;
+            int bin = 0b_0010_1010;
+
+            String name=String.Empty;
+
+            name+="Jjumba";
+
+            char [] letters={'h','e','l','l','o'};
+
+            String magic=string.Format("jkk {0}",23);
+
+            String derived=new String(letters);
+
+            Console.WriteLine(magic.ToUpper());
+            /* Date Time*/
+
+
+            DateTime dt = new DateTime(2019, 12, 25);
+
+            /*Object boxing and unboxing*/
+            int y=7;
+
+            object benji=y;
+
+            int z=(int)benji;
+            /*make a value nullable*/
+            int? c=42;
+
+            /*Check whether has a value*/
+
+            Console.WriteLine(c.HasValue);
+
+
+
+            Console.WriteLine(z);
+            
+
+
+            Console.WriteLine(dt);
+        }
+    }
+}
